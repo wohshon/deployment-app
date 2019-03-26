05d586beec0c62495b1fc8716f58bc7cafbf97d7
EC2 Default User <ec2-user@ip-10-0-1-225.ap-southeast-1.compute.internal>
2019-03-26 06:21:15 +0000

    updated version

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
 	var ip = require("ip");
 	console.dir ( ip.address() );
 	//res.sendfile("views/index.html");
-  res.render('index', { title: "demo", ipaddr: ip.address(), version: "v1" });
+  res.render('index', { title: "demo", ipaddr: ip.address(), version: "v2" });
 });
 
 router.get('/rest', function(req, res, next) {
